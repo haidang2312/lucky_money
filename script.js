a041d8653e8fc7c569030c9af19ea7c37110c177
Nguyen Hai Dang <danghai23122005@gmail.com>
2025-02-02 22:51:47 +0700

    a

--- script.js
+++ script.js
@@ -71,3 +71,12 @@ document.getElementById('envelope').addEventListener('click', function() {
         });
     }
 });
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
+        event.preventDefault();
+    }
+});
